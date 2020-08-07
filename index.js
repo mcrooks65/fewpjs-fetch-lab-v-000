8a837e2e8cf81ff361da01cae5ec75f37650a560
Matthew Crooks <mfcrooks@gmail.com>
2020-08-07 20:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function fetchBooks() {
   .then(json => renderBooks(json));
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+// function renderBooks(json) {
+//   const main = document.querySelector('main')
+//   json.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = `<h2>${book.name}</h2>`
+//     main.appendChild(h2)
+//   })
+// }
+// 
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
